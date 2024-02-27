763656a519c9ddc3dc711297a0b90dd9fb913173
Kuldeep Lodha <kuldeeplodha04@gmail.com>
2024-02-27 22:28:55 +0530

    updated Portfolio.jjs

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,27 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Kuldeep Lodha",
+  title: "Hi all, I'm Kuldeep",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "Passionate Python developer with strong software development skills. Experienced in Django, Pandas, NumPy, and various Python frameworks. Proficient in web app development, data analysis, machine learning, and NLP. Committed to ongoing learning and staying updated on industry trends."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1CR4r5hGWw1JvmHZiWtMXCXJDGCx17nzm/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/kuldeeplodha",
+  linkedin: "www.linkedin.com/in/kuldeeplodha",
+  gmail: "kuldeeplodha04@gmail.com",
+  kaggle:"https://www.kaggle.com/kuldeeplodha",
+  //gitlab: "https://gitlab.com/saadpasta",
+  //facebook: "https://www.facebook.com/saad.pasta7",
+  //medium: "https://medium.com/@saadpasta",
+  //stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
